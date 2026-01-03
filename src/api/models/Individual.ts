8651a93361e14c6fafb120ea3c6010352ba997ee
Eika <65638670+eika-yamashita@users.noreply.github.com>
2026-01-03 09:44:14 +0900

    Align individual list with backend schema

--- src/api/models/Individual.ts
+++ src/api/models/Individual.ts
@@ -1,6 +1,28 @@
 export interface Individual {
-    speciesCd: string;
-    id: string;
-    name: string;
-    gender: string;
-  }
+  species_cd: string;
+  id: string;
+  male_parent_id?: string;
+  female_parent_id?: string;
+  morph?: string;
+  bloodline?: string;
+  gender_category?: string;
+  breeding_category?: string;
+  breeder?: string;
+  clutch_date?: string;
+  hatch_date?: string;
+  purchase_from?: string;
+  purchase_price?: number;
+  purchase_date?: string;
+  sales_category?: string;
+  sales_to?: string;
+  sales_price_tax_ex?: number;
+  sales_price_tax?: number;
+  sales_price_tax_in?: number;
+  sales_date?: string;
+  death_date?: string;
+  note?: string;
+  create_user: string;
+  create_at: string;
+  update_user?: string;
+  update_at?: string;
+}
